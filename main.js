610da6a432e1eabf377c212b6af36cc16d53194e
+ <+>
2022-08-01 21:18:17 -0400

    commit tree 6a526ee0df2b7d112bf30ace07501fdc8803ed1c

--- main.js
+++ main.js
@@ -1,10 +1,32 @@
 // Defining text characters for the empty and full hearts for you to use later.
+
+
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+document.addEventListener('DOMContentLoaded', () => {
+  const heart = document.querySelectorAll('span.like-glyph');
+  const errorMsg = document.getElementById('modal')
+  heart.forEach(heart => heart.addEventListener('click', cb))
 
+  function cb(heart){
+    if (heart.target.innerText === EMPTY_HEART) {
+      heart.target.innerText =  FULL_HEART
+      heart.target.classList.add('activated-heart')
+    } else if (heart.target.innerText === FULL_HEART) {
+      heart.target.innerText = EMPTY_HEART
+      heart.target.classList.remove('activated-heart')
+    }
+    mimicServerCall()
+    .catch(() => {
+      errorMsg.className = '';
 
+      setTimeout(() => {
+        errorMsg.className = 'hidden'
+      }, 3000)
+    })
+  }
+})
 
 
 //------------------------------------------------------------------------------
@@ -23,3 +45,38 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
     }, 300);
   });
 }
+
+
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   // Your JavaScript code goes here!
+//   const errorMsg = document.getElementById('modal')
+//   errorMsg.classList.add('hidden')
+//   const heart = document.querySelectorAll('span.like-glyph')
+  
+  
+//   heart.forEach(heart => heart.addEventListener('click', cb))
+  
+//   function cb(heart) {
+//     mimicServerCall()
+//     .then(() => {
+//       if (heart.target.innerText === EMPTY_HEART) {
+//             heart.target.innerText =  FULL_HEART
+//             heart.target.classList.add('activated-heart')
+//       } else if
+//         (heart.target.innerText === FULL_HEART) {
+//           heart.target.innerText = EMPTY_HEART
+//           heart.target.classList.remove('activated-heart')
+//         }
+//       })
+//       .catch(() => {
+        
+//         errorMsg.className = '';
+//         console.log(errorMsg)
+        
+//         setTimeout(() => {
+//           console.log(errorMsg)
+//           errorMsg.className = 'hidden'}, 3000)
+//       })
+//     }
+//   })
